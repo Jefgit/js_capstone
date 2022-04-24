2c3ded9ad0ff86a1340a49a17cf346083e5f7871
Your Name <riazondajf@gmail.com>
2022-04-24 22:27:43 +0800

    Added upload image filter feature and large Improvement on UI/UX

--- app.js
+++ app.js
@@ -32,17 +32,52 @@ app.set('views', __dirname + '/views');
 
 app.set('view engine', 'ejs');
 
-app.use(homeRoutes);
+// app.use(homeRoutes);
+var files = fs.readdirSync('./assets/images/backgrounds/');
+let message = "";
+let error = "";
 
-// app.get('/', (req, res) =>{
-//     render
-// })
+for(let i = 0; i < files.length; i++){
+    files[i] = files[i].replace("_bg.jpg", "");
+}
+console.log(files);
 
-// app.post('/', (req, res) => {
-//     if(req.files) {
-//         console.log(req);
-//     }
-// })
+app.get('/', (req, res) =>{
+    res.render('index', {files, message, error});
+})
+
+app.post('/', (req, res) => {
+    
+
+    if(req.files) {
+        console.log(req.files);
+        var file = req.files.file;
+        var filename = file.name;
+        console.log(filename);
+        let format = filename.split("_");
+
+        if( format[1] && format[1] == "bg.jpg"){      
+            file.mv('./assets/images/backgrounds/'+filename, function(err){
+                if (err){
+                    error = err;
+                    res.render('index', {files, message, error});
+                }
+                else{
+                    message = "Successfully uploaded";
+                    res.render('index', {files, message, error});
+                }
+            })
+        }
+        else{
+            error = "Invalid filename or file format";
+            res.render('index', {files, message, error});
+        }
+    }
+    else{
+        error = "No image file selected.";
+        res.render('index', {files, message, error});
+    }
+})
 
 
 app.listen(8000, function(){
--- assets/scripts/func.js
+++ assets/scripts/func.js
@@ -1,9 +1,20 @@
 //for webrtc
 let mediaRecorder, recordedBlobs;
 let img_name;
+let img_exist;
+let is_recording;
 let displayVideo      = document.querySelector('video#video');
+const checkbox        = document.getElementById('checkbox');
+const myForm          = document.getElementById('myForm');
+const file            = document.getElementById('file');
+const watch           = document.getElementById('watch');
+const icon_upload     = document.getElementById('icon_upload');
+const upload          = document.getElementById('upload');
+const image           = document.getElementById('image');
+const add_bg          = document.getElementById('add_bg');
 const videoElement    = document.getElementById('video');
 const canvas          = document.getElementById('canvas');
+const output          = document.getElementById('output');
 const background      = document.getElementById('background');
 const errorMsgElement = document.querySelector('span#errorMsg');
 const recordedVideo   = document.querySelector('video#recorded');
@@ -15,21 +26,113 @@ const snapButton      = document.querySelector('button#screenShot');
 
 const ctxImg = canvas.getContext('2d');
 
+file.onchange =  function(){
+  var src = URL.createObjectURL(this.files[0]);
+  image.src = src;
+}
+
+add_bg.addEventListener('click', async () => {
+  if(myForm.style.display == "block"){
+    myForm.style.display = "none";
+  }
+  else{
+    myForm.style.display = "block";
+  }
+
+});
+
+watch.addEventListener('click', async () => {
+  if(output.style.display == "block"){
+    output.style.display = "none";
+
+    if(recordedBlobs){
+    const superBuffer = new Blob(recordedBlobs, {type: 'video/webm'});
+    recordedVideo.src = null;
+    recordedVideo.srcObject = null;
+    recordedVideo.src = window.URL.createObjectURL(superBuffer);
+    try {
+      recordedVideo.stop();
+    } catch (error) {
+      console.log(error);
+    }
+    
+    }
+    
+  }
+  else{
+    const superBuffer = new Blob(recordedBlobs, {type: 'video/webm'});
+    recordedVideo.src = null;
+    recordedVideo.srcObject = null;
+    recordedVideo.src = window.URL.createObjectURL(superBuffer);
+    output.style.display = "block";
+  }
+
+});
+
+icon_upload.addEventListener('click', async () => {
+ file.click();
+});
+
+checkbox.addEventListener('change', async () => {
+  if(checkbox.checked){
+    recordButton.disabled = false;
+    snapButton.disabled = false;
+     // startButton.disabled = true;
+        // const hasEchoCancellation = document.querySelector('#echoCancellation').checked; 
+        const constraints = {
+          audio: {
+            // echoCancellation: {exact: hasEchoCancellation}
+          },
+          video: {
+            width: 480, height: 320  //set video screen dimension
+          }
+        };
+        // console.log('Using media constraints:', constraints);
+      await init(constraints);
+
+  }
+  else{
+    window.stream.getTracks().forEach(function(track) {
+      track.stop();
+      displayVideo.srcObject = null;
+    });
+    // stream.getTracks().forEach(track => track.stop())
+    // startButton.textContent = 'Start Camera';
+     recordButton.disabled = true;
+    snapButton.disabled = true;
+
+  }
+  
+});
 //when start button camera is clicked open camera view
-startButton.addEventListener('click', async () => {
-    startButton.disabled = true;
-    // const hasEchoCancellation = document.querySelector('#echoCancellation').checked; 
-    const constraints = {
-      audio: {
-        // echoCancellation: {exact: hasEchoCancellation}
-      },
-      video: {
-        width: 480, height: 320  //set video screen dimension
-      }
-    };
-    // console.log('Using media constraints:', constraints);
-    await init(constraints);
-  });
+// startButton.addEventListener('click', async () => {
+
+//   if (startButton.textContent === 'Close Camera') {
+//     window.stream.getTracks().forEach(function(track) {
+//       track.stop();
+//       displayVideo.srcObject = null;
+//     });
+//     // stream.getTracks().forEach(track => track.stop())
+//     startButton.textContent = 'Start Camera';
+//      recordButton.disabled = true;
+//     snapButton.disabled = true;
+//   }
+//   else{
+   
+//     // startButton.disabled = true;
+//         // const hasEchoCancellation = document.querySelector('#echoCancellation').checked; 
+//       const constraints = {
+//         audio: {
+//           // echoCancellation: {exact: hasEchoCancellation}
+//         },
+//         video: {
+//           width: 480, height: 320  //set video screen dimension
+//         }
+//       };
+//       // console.log('Using media constraints:', constraints);
+//     await init(constraints);
+//   }
+//   });
 
 async function init(constraints) {
     try {
@@ -42,20 +145,40 @@ async function init(constraints) {
   }
 
 function handleSuccess(stream) {
-  recordButton.disabled = false;
-  snapButton.disabled = false;
-
   console.log('getUserMedia() got stream:', stream);
-  window.stream = stream;
+    window.stream = stream;
   
   // const displayVideo = document.querySelector('video#display');
   displayVideo.srcObject = stream;
-  videoElement.play();
 
+  // if (startButton.textContent === 'Close Camera') {
+  //   window.stream.getTracks().forEach(function(track) {
+  //     track.stop();
+  //     displayVideo.srcObject = null;
+  //   });
+  //   // stream.getTracks().forEach(track => track.stop())
+  //   startButton.textContent = 'Start Camera';
+  //    recordButton.disabled = true;
+  //   snapButton.disabled = true;
+  // }
+  // else{
+    recordButton.disabled = false;
+    snapButton.disabled = false;
+    // startButton.textContent = 'Close Camera';  
+    videoElement.play();
+  // }
 }
 
-//when Take Snapshot button is clicked
-snapButton.addEventListener('click', () => {
+
+function stopStream(stream) { //close camera
+  stream.getTracks().forEach(function(track) {
+      if (track.readyState == 'live') {
+          track.stop();
+      }
+  });
+}
+
+snapButton.addEventListener('click', () => { //when Take Snapshot button is clicked
   if($('#video').is(":hidden")){
     takeSnapshot();
   }
@@ -66,16 +189,40 @@ snapButton.addEventListener('click', () => {
 })
 
 //when record button is clicked
-recordButton.addEventListener('click', () => {
-  if (recordButton.textContent === 'Record') {
+// recordButton.addEventListener('click', () => {
+//   if (recordButton.textContent === 'Record') {
+//     startRecording();
+//   } else {
+//     stopRecording();
+//     watch.style.color = "red";
+//     recordButton.textContent = 'Record';
+//     playButton.disabled = false;
+//     downloadButton.disabled = false;
+//     output.hidden = false;
+//   }
+// });
+
+$('#record').addClass("notRec");
+
+$('#record').click(function(){
+	if($('#record').hasClass('notRec')){ //record
+		$('#record').removeClass("notRec");
+		$('#record').addClass("Rec");
     startRecording();
-  } else {
+	}
+	else{//stop recording
+		$('#record').removeClass("Rec");
+		$('#record').addClass("notRec");
+
     stopRecording();
-    recordButton.textContent = 'Record';
+    watch.style.color = "red";
+    // recordButton.textContent = 'Record';
+    is_recording = false;
     playButton.disabled = false;
     downloadButton.disabled = false;
-  }
-});
+    output.hidden = false;
+	}
+});	
 
 //for recording video
 function startRecording() {
@@ -90,7 +237,8 @@ function startRecording() {
   }
   
   console.log('Created MediaRecorder', mediaRecorder, 'with options', options);
-  recordButton.textContent = 'Stop Recording';
+  // recordButton.textContent = 'Stop Recording';
+  is_recording = true;
   playButton.disabled = true;
   downloadButton.disabled = true;
   mediaRecorder.onstop = (event) => {
@@ -116,7 +264,7 @@ function takeSnapshot(){
     .then(canvas1 => {
       const image = canvas1.toDataURL('image/jpg').replace('image/jpg', 'image/octet-stream')
       const a = document.createElement('a')
-      a.setAttribute('download', 'Screenshot.jpg')
+      a.setAttribute('download', "Screenshot"+Date.now()+".jpg")
       a.setAttribute('href', image)
       a.click()
       canvas1.remove()
@@ -127,9 +275,7 @@ function takeSnapshot(){
     let ctx = canvas.getContext('2d');
     canvas.width = displayVideo.videoWidth;
     canvas.height = displayVideo.videoHeight;
-
     ctx.drawImage(displayVideo,0,0);
-    console.log(canvas);
     return new Promise((res,rej) =>{
     canvas.toBlob(res,"image/jpeg"); //assign filetype
     })
@@ -170,11 +316,11 @@ function stopRecording() {
 
 //for replaying recorded video
 playButton.addEventListener('click', () => {
-    const superBuffer = new Blob(recordedBlobs, {type: 'video/webm'});
-    recordedVideo.src = null;
-    recordedVideo.srcObject = null;
-    recordedVideo.src = window.URL.createObjectURL(superBuffer);
-    recordedVideo.controls = true;
+    // const superBuffer = new Blob(recordedBlobs, {type: 'video/webm'});
+    // recordedVideo.src = null;
+    // recordedVideo.srcObject = null;
+    // recordedVideo.src = window.URL.createObjectURL(superBuffer);
+    // recordedVideo.controls = true;
     recordedVideo.play();
 });
 
@@ -185,9 +331,13 @@ downloadButton.addEventListener('click', () => {
   const a = document.createElement('a');
   a.style.display = 'none';
   a.href = url;
-  a.download = 'test.mp4';
+  a.download = "viduo"+Date.now()+".mp4";
   document.body.appendChild(a);
   a.click();
+  output.hidden = true;
+  recordedVideo.src = null;
+  recordedBlobs = [];
+  watch.style.color = "black";
   setTimeout(() => {
     document.body.removeChild(a);
     window.URL.revokeObjectURL(url);
@@ -197,9 +347,10 @@ downloadButton.addEventListener('click', () => {
 //for speech recognition
 var speechRecognition = window.webkitSpeechRecognition;
 var recognition = new speechRecognition();
-var textbox = $("#textbox");
+var textbox = $("#speech");
 var instruction = $("#instruct");
 var content = "";
+// var stat = "";
 
 recognition.continuos = true;
 
@@ -208,7 +359,12 @@ recognition.onstart = () => {
 }
 
 recognition.onspeechend = () => {
-  instruction.text("No Activity");
+  // if(stat){
+  //   instruction.text(stat);
+  // }
+  // else{
+    instruction.text("No Activity");
+  // }
 }
 
 recognition.onerror = () => {
@@ -221,43 +377,62 @@ recognition.onresult = (event) => {
   var transcript = event.results[current][0].transcript;
 
   content = transcript;
+  textbox.text(content);
 
-  textbox.val(content);
-  console.log("yes");
-  let words = textbox.val().split(' ');
-  let path = "url('../images/"+words[1]+"_bg.jpg')";
-  console.log( checkImage("../images/"+words[1]+"_bg.jpg"));
+  let words = textbox.text().split(' ');
+  let path = "url('../images/backgrounds/"+words[1]+"_bg.jpg')";
 
-  if(textbox.val() == "snap"){
-    if($('#video').is(":hidden")){
-      takeSnapshot();
-    }
-    else{
-      takeSnapshot()
-      .then(downloadPic);
-    }
-  }
-  else if(textbox.val() == "blur background"){
+  // if(textbox.text() == "snap"){
+  //   if($('#video').is(":hidden")){
+  //     takeSnapshot();
+  //   }
+  //   else{
+  //     takeSnapshot()
+  //     .then(downloadPic);
+  //   }
+  // }
+  // else 
+  if(textbox.text() == "blur background"){
+    // stat = "Blurry background activated";
     background.hidden = true;
     displayVideo.hidden = true;
     canvas.hidden = false;
     loadBodyPix();
   }
   else if(words[0] == "background"){
-   
-    img_name = words[1];
-    console.log( img_name);
-    
-    background.style.backgroundImage = path;
-    background.hidden = false;
-    displayVideo.hidden = true;
-    canvas.hidden = false;
-    canvas.width = videoElement.width;
-    canvas.height = videoElement.height;
+    // stat = "";
+    checkImage("../images/backgrounds/"+words[1]+"_bg.jpg")
+    .then((value) => {
+      console.log(value);
+      if(value){
+        // stat = "Background filter changed.";
+        img_name = words[1];
+      console.log( img_name);
+      
+      background.style.backgroundImage = path;
+      background.hidden = false;
+      displayVideo.hidden = true;
+      canvas.hidden = false;
+      canvas.width = videoElement.width;
+      canvas.height = videoElement.height;
 
-    ctxImg.drawImage(videoElement, 0, 0);
-    removebackground();
-  
+      ctxImg.drawImage(videoElement, 0, 0);
+      removebackground();
+      }
+      else{
+        // stat = "Background filter not available";
+        background.hidden = true;
+        displayVideo.hidden = false;
+        canvas.hidden = true;
+      }
+    });
+  }
+  else if(textbox.text() == "object"){
+    canvas.hidden = false;
+    canvas.width = 550;
+    canvas.height = 500;
+    displayVideo.hidden = true;
+      insertObject();
   }
   else{
     background.hidden = true;
@@ -267,15 +442,13 @@ recognition.onresult = (event) => {
 
 }
 
-function checkImage(path){
-  var image = new Image();
-  var url_image = path;
-  image.src = url_image;
-  if (image.width == 0) {
-    return false;
-  } else {
-    return true;
-  }
+async function checkImage(url){
+     
+  const res = await fetch(url);
+  const buff = await res.blob();
+ 
+  return buff.type.startsWith('image/');
+
 }
 
 $("#start-btn").click((event) => {
@@ -287,7 +460,7 @@ $("#start-btn").click((event) => {
 })
 
 textbox.on('input', () => {
-  content = $(this).val();
+  content = $(this).text();
 });
 
 displayVideo.onplaying = () => {
@@ -309,38 +482,40 @@ function loadBodyPix() {
 
 async function perform(net) {
   
-    while (textbox.val() == "blur background") {
-      const segmentation = await net.segmentPerson(video);
+  while (textbox.text() == "blur background") {
+    const segmentation = await net.segmentPerson(video);
 
-      const backgroundBlurAmount = 5;
-      const edgeBlurAmount = 2;
-      const flipHorizontal = false;
+    const backgroundBlurAmount = 5;
+    const edgeBlurAmount = 2;
+    const flipHorizontal = false;
 
-      bodyPix.drawBokehEffect(
-        canvas, videoElement, segmentation, backgroundBlurAmount,
-        edgeBlurAmount, flipHorizontal);
-    }
+    bodyPix.drawBokehEffect(
+      canvas, videoElement, segmentation, backgroundBlurAmount,
+      edgeBlurAmount, flipHorizontal);
   }
+}
 
-  async function removebackground() {
+async function removebackground() {
 
   const canvas = document.querySelector('canvas');
   const ctx = canvas.getContext('2d');
 
-  while (textbox.val() == "background "+img_name) {
-  // Loading the model
-  const net = await bodyPix.load({
-    multiplier: 0.75,
-    stride: 32,
-    quantBytes: 4
-  });
+  while (textbox.text() == "background "+img_name) {
+    // stat = "background "+img_name+" activated";
+
+    // Loading the model
+    const net = await bodyPix.load({
+      multiplier: 0.75,
+      stride: 32,
+      quantBytes: 4
+});
   
-  // Segmentation
-  const { data:map } = await net.segmentPerson(videoElement, {
-    flipHorizontal: true,
-    internalResolution: 'medium',
-    segmentationThreshold: 0.6
-  });
+    // Segmentation
+    const { data:map } = await net.segmentPerson(videoElement, {
+      flipHorizontal: true,
+      internalResolution: 'medium',
+      segmentationThreshold: 0.6
+    });
   
   ctx.drawImage(videoElement, 0, 0, videoElement.width, videoElement.height);
   var imageData = ctx.getImageData(0,0, videoElement.width, videoElement.height);
@@ -356,4 +531,13 @@ async function perform(net) {
   ctx.imageSmoothingEnabled = true;
   ctx.putImageData(imageData, 0, 0);
   }
+}
+
+function insertObject(){
+  let canvas3 = document.createElement('canvas');
+  let ctx2 = canvas3.getContext('2d');
+  var img = new Image;
+  img.src = '../images/objects/apple.png';
+  // document.appendChild(img);
+  ctx2.drawImage(img,10,10);
 }
